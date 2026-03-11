bd6a9700041768637ecc326058983363b1641bb0
aybeniz-24 <elesgerovaaybeniz526@gmail.com>
2026-03-11 21:01:20 +0400

    header and footer refresh2

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,4 @@
+// slider
 var swiper = new Swiper(".headerSwiper", {
     loop: true,
 
@@ -12,8 +13,51 @@ var swiper = new Swiper(".headerSwiper", {
 });
 
 
-const lang = document.querySelector(".langDropdown");
 
+
+// dil secimi
+const lang = document.querySelector(".langDropdown");
 lang.addEventListener("click", () => {
     lang.classList.toggle("active");
 });
+
+
+
+
+// submenu
+let dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach(item => {
+  item.addEventListener("click", function(e){
+    e.preventDefault();
+
+
+    let submenu = this.querySelector(".submenu");
+    let icon = this.querySelector("i");
+    let isOpen = submenu.classList.contains("active");
+
+    // bütün submenuları bağla
+    document.querySelectorAll(".submenu").forEach(menu=>{
+      menu.classList.remove("active");
+    });
+
+    // bütün ikonları reset et
+    document.querySelectorAll(".dropdown i").forEach(i=>{
+      i.classList.remove("fa-caret-up");
+      i.classList.add("fa-caret-down");
+    });
+
+    // əgər klik olunan əvvəl açıq deyildisə aç
+    if(!isOpen){
+      submenu.classList.add("active");
+      icon.classList.remove("fa-caret-down");
+      icon.classList.add("fa-caret-up");
+    }
+
+  });
+});
+
+
+// copyright
+let year = new Date().getFullYear();
+document.getElementById("year").textContent = year;
